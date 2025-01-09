6c58447a5f7a62b434b56a8453981bee4c825b7b
Bakhadirov-Jaloliddin-25 <jboxodirov4@gmail.com>
2025-01-09 08:28:21 +0500

    first commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,37 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  const pathname = usePathname();
+
+  const activeClassName = (currentPath: string) => {
+    if (currentPath === pathname) return "text-amber-500";
+    return "";
+  };
+
+  return (
+    <div className="flex gap-5 p-5">
+      <span>Header</span>
+      <Link className={`font-semibold ${activeClassName("/")}`} href={"/"}>
+        Home
+      </Link>
+      <Link
+        className={`font-semibold ${activeClassName("/about")}`}
+        href={"/about"}
+      >
+        About
+      </Link>
+      <Link
+        className={`font-semibold ${activeClassName("/contact")}`}
+        href={"/contact"}
+      >
+        Contact
+      </Link>
+    </div>
+  );
 };
 
 export default Header;
